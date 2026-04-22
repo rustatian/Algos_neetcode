fddbb071aa1b967f3ced03cc776db9f9cc55c91f
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-04-22 16:59:56 +0200

    Add: find-peak-element - submission-0

---
+++ Data Structures & Algorithms/find-peak-element/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+                return mid
+            elif nums[mid] > nums[mid + 1] and nums[mid] <= nums[mid - 1]:
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
